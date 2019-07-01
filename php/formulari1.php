bc936ffc970e8614d3cfa6573ea22d889ba82f23
Marshall-Power <marcel.artemis@gmail.com>
2019-07-01 12:00:57 +0200

    Created formular1
    A form that sends a number to another php page that shows the multiplication table

---
+++ php/formulari1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+  <script src="../jquery.js"></script>
+  <title>Formularis</title>
+</head>
+<body>
+  <form method="POST" action="primerphp.php" id="formulari">
+    Número:
+    <input type="number" name="num">
+    <br>
+    <input type="submit">
+  </form>
+  
+</body>
+</html>
