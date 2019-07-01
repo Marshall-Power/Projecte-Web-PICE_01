218de5506bd36b6f781240c2cecab356d8b62315
Marshall-Power <marcel.artemis@gmail.com>
2019-07-01 12:02:58 +0200

    Created formulari2
    Created exercice that given a number and a food choice sends the data to antother pge where it is shown a picture (x times the number we choose)

---
+++ php/formulari2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Plats a escollir</title>
+  <script>
+    var $select = $("select");
+    $select.on("change", function() {
+        var selected = [];  
+        $.each($select, function(index, select) {           
+            if (select.value !== "") { selected.push(select.value); }
+        });         
+      $("option").prop("disabled", false);         
+      for (var index in selected) { $('option[value="'+selected[index]+'"]').prop("disabled", true); }
+    });
+  </script>
+</head>
+<body>
+  <form method="GET" action="menjars.php" id="formulari">
+    <input type="number" name="n1"> 
+    <select name="selec1">
+      <option value="" disabled="" selected="" style="display:none;">Tría un plat</option>
+      <option value="0">Pizza</option>
+      <option value="1">Kebab</option>
+      <option value="2">Pasta</option>
+      <option value="3">Arros</option>
+      <option value="4">Sopa</option>
+    </select>
+    <br>
+    <input type="number" name="n2">
+    <select name="selec2">
+      <option value="" disabled="" selected="" style="display:none;">Tría un plat</option>
+      <option value="0">Pizza</option>
+      <option value="1">Kebab</option>
+      <option value="2">Pasta</option>
+      <option value="3">Arros</option>
+      <option value="4">Sopa</option>
+    </select>
+    <br>
+    <input type="number" name="n3">
+    <select name="selec3">
+      <option value="" disabled="" selected="" style="display:none;">Tría un plat</option>
+      <option value="0">Pizza</option>
+      <option value="1">Kebab</option>
+      <option value="2">Pasta</option>
+      <option value="3">Arros</option>
+      <option value="4">Sopa</option>
+    </select>
+    <br>
+    <input type="number" name="n4">
+    <select name="selec4">
+      <option value="" disabled="" selected="" style="display:none;">Tría un plat</option>
+      <option value="0">Pizza</option>
+      <option value="1">Kebab</option>
+      <option value="2">Pasta</option>
+      <option value="3">Arros</option>
+      <option value="4">Sopa</option>
+    </select>
+    <br>
+    <input type="number" name="n5">
+    <select name="selec5">
+      <option value="" disabled="" selected="" style="display:none;">Tría un plat</option>
+      <option value="0">Pizza</option>
+      <option value="1">Kebab</option>
+      <option value="2">Pasta</option>
+      <option value="3">Arros</option>
+      <option value="4">Sopa</option>
+    </select>
+    <br>
+    <input type="submit">
+  </form>
+</body>
+</html>
