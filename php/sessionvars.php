e1fd8b09e3cfd41136dd3e1e024839ac08dc9b7b
Marshall-Power <marcel.artemis@gmail.com>
2019-07-01 13:21:29 +0200

    Created a index and landing page to test sesison variables

---
+++ php/sessionvars.php
@@ -0,0 +1,14 @@
+<?php session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Inicial</title>
+</head>
+<body>
+  <?php $_SESSION["NOM"]="Marcel";?>
+</body>
+</html>
