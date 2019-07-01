3a917144fe216264993795e730b818f1a6a64a9c
Marshall-Power <marcel.artemis@gmail.com>
2019-07-01 09:16:41 +0200

    Created primerphp.php
    Arxiu per començar a testejar en php

---
+++ php/primerphp.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <h1> titol principal </h1>
+  <p>Qui ea reprehenderit aliquip adipisicing laboris irure id est in. Ex ea irure et ex irure Lorem eu duis pariatur eiusmod nulla. Excepteur velit est esse proident mollit ex sit. Ex nisi deserunt do labore aliqua nostrud pariatur nulla aliquip minim culpa quis minim.
+
+      Proident consectetur irure reprehenderit consectetur quis sunt ut ad. Qui dolore laborum pariatur esse anim id commodo enim incididunt cupidatat. Exercitation nulla fugiat adipisicing aute nostrud sint veniam. Occaecat incididunt aute commodo proident aute occaecat. Esse eu qui eu officia pariatur ipsum do adipisicing ullamco excepteur exercitation tempor velit dolore. Sint aliquip culpa aliqua consectetur eu sunt ad.
+
+      Aliqua magna in voluptate minim eiusmod proident enim irure labore quis proident deserunt ex. Incididunt consequat et veniam nisi ullamco excepteur aliquip pariatur. Commodo pariatur ut dolor adipisicing. Aliquip qui minim cupidatat veniam nulla eu laboris.</p>
+  <?php echo "Hola";  ?>
+
+</body>
+</html>
