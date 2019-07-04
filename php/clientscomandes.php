f962d8052d1a987d51f8e1006a766d43b4ec0087
Marshall-Power <marcel.artemis@gmail.com>
2019-07-04 12:18:56 +0200

    create clientscomandes.php
    This file will show customers orders when clicking on the customer name in clientsindex.php

---
+++ php/clientscomandes.php
@@ -0,0 +1,40 @@
+<?php
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "proves";
+
+  $con = new mysqli($server,$username,$password,$database);
+  if($con->connect_error){
+    die ("Error al connectarse".$con->connect_error);
+  }
+
+  $clientid = $_GET["clientid"];
+  $query = "SELECT * FROM orders WHERE CustomerID ='$clientid'";
+  $result = $con->query($query);
+  if ($result->num_rows==0){
+    echo "No s'han trobat dades";
+  }else{ 
+    echo (
+      "<table class='table table-striped'>
+      <caption> <u>Comandes del client seleccionat <u></caption> 
+      <thead>
+        <tr>
+          <th>OrderID</th>
+          <th>OrderDate</th>
+          <th>Address</th>
+          <th>City</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody>    
+    ");
+    while($client = $result->FETCH_ASSOC()){
+      echo utf8_encode("<tr taula-id='2'><td>".$client["OrderID"]."</td><td>".$client["OrderDate"]."</td><td>".$client["ShipAddress"]."</td><td>".$client["ShipCity"]."</td><td>".$client["ShipCountry"]."</td></tr>");     
+    }
+    echo "</tbody></table>";
+  }
+  
+  $con->close();
+
+?>
