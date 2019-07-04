0140db28157416b525c829f9a6c9ed04b584f70d
Marshall-Power <marcel.artemis@gmail.com>
2019-07-04 09:06:04 +0200

    updated empleats
    updated so you can chose the city as a filter

--- php/empleats.php
+++ php/empleats.php
@@ -20,39 +20,85 @@
   <script src="../jquery.js"></script>
   <title>Empleats SQL Test</title>
   <style>
-    #navbar{
-      text-align: center;
-      position:sticky;
-      bottom: 0px;
-      margin-bottom:10px;
-    }
+    
   </style>
+  <script>
+    $(document).ready(function(){
+      $('#selpais').on('change', function(){
+        $("#triarpais").remove();  
+        var pais = this.value;
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("container").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","empleatspais.php?pais="+pais,true);
+        xmlhttp.send();
+      });
+    });
+    
+    function triarciutat(country){
+        $('#selectciutat').on('change', function(){
+        var ciutat = this.value;
+        var pais = country;
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("container").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","empleatspais.php?pais="+pais+"&ciutat="+ciutat,true);
+        xmlhttp.send();
+         
+        });
+      }
+    
+  </script>
 </head>
-<body>
-
-  <table class="table">
-  <thead>
-    <tr>
-      <th>Títol</th>
-    </tr> 
-  </thead>
-  <tbody> 
-    <?php
-        $query = "SELECT Title, COUNT(Title) AS Quantitat FROM employees GROUP BY Title";
+<body> 
+  <div id="selectors">
+    <select class="select" id="selpais">
+      <option id="triarpais"> Tria un país </option>
+      <?php
+        $query = "SELECT DISTINCT(Country) FROM customers";
         $result = $con->query($query);
         if ($result->num_rows==0){
           echo "No s'han trobat dades";
-        }else{        
-          while($client = $result->FETCH_ASSOC()){   
-            echo utf8_encode($client["Title"]." ");       
-            echo utf8_encode($client["Quantitat"]."</br>");
+        }else{      
+          while($client = $result->FETCH_ASSOC()){ 
+            echo "<option value=".$client["Country"].">".$client["Country"]."</option>";
           }
         }
-
-      
-      
-    ?>
+      ?>
+    </select>    
+  </div>
   
+  <div id="container">
+    <table class="table">
+    <thead>
+      <tr>
+        <th>Nom</th>
+        <th>Títol</th>
+        <th>Ciutat</th>
+        <th>País</th>
+      </tr> 
+    </thead>
+    <tbody> 
+      <?php
+          $query = "SELECT * FROM customers";
+          $result = $con->query($query);
+          if ($result->num_rows==0){
+            echo "No s'han trobat dades";
+          }else{        
+            while($client = $result->FETCH_ASSOC()){   
+              echo utf8_encode("<tr><td>".$client["ContactName"]."</td><td>".$client["ContactTitle"]."</td><td>".$client["City"]."</td><td>".$client["Country"]."</td></tr>");  
+            }
+          }
+      ?>
+    </tbody>
+    <table>
+  </div>
 </body>
 </html>
 <?php
