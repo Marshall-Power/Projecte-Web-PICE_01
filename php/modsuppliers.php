b7e837f3df49acd3781bb005b557ae9faa2f5c8d
Marshall-Power <marcel.artemis@gmail.com>
2019-07-08 09:06:31 +0200

    Fixed some variable names

--- php/modsuppliers.php
+++ php/modsuppliers.php
@@ -9,15 +9,15 @@ if (isset($_POST["suppid"])){
 
   echo "<label for='mnomcomp'>Nom de la companyia: </label><br>
   <input type='text' id='mnomcomp' value='".$resultat["CompanyName"]."' autofocus><br>
-  <label for='mnomcontacte'>Nom del contacte: </label>
+  <label for='mnomcontacte'>Nom del contacte: </label> <br>
   <input type='text' id='mnomcontacte' value='".$resultat["ContactName"]."'><br>
   <label for='madress'>Adreça: </label><br>
   <input type='text' id='madress' value='".$resultat["Address"]."'><br>
   <label for='mciutat'>Ciutat: </label><br>
-  <input type='text' id='mciutat' value='".$resultat["ContactName"]."'><br>
+  <input type='text' id='mciutat' value='".$resultat["City"]."'><br>
   <label for='mpais'>Pais: </label><br>
-  <input type='text' id='mpais' value='".$resultat["ContactName"]."'><br>
-  <button class='btn btn-danger' id='comit'>Modificar dades</button>
+  <input type='text' id='mpais' value='".$resultat["Country"]."'><br>
+  <button class='btn btn-danger' id='modificar'>Modificar proveidor</button>
   ";
   }
 
