e194e65af8880412b6ace934c3343a73703566be
Marshall-Power <marcel.artemis@gmail.com>
2019-07-10 13:27:29 +0200

    Creat fitxerpjarxius
    Creat per pujar un fitxer

---
+++ php/pujararxius.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css">
+  <script src="../jquery.js"></script>
+  <title>Document</title>
+</head>
+<body>
+  <form method="POST" enctype="multipart/form-data" action="rebrearxius.php">
+    <label for="file">Foto</label>
+    <input type="file" id="file" name="foto">
+    <button type="submit" class="btn btn-info">Enviar</button>
+  </form>
+  
+</body>
+</html>
